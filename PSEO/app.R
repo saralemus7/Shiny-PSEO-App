b783ebd9b317d6f3dd003677d791eeb7961b4fa1
saralemus7 <saraalemus70@gmail.com>
2020-06-21 22:01:26 -0700

    more changes, fixed graph

--- PSEO/app.R
+++ PSEO/app.R
@@ -38,26 +38,24 @@ ui <- fluidPage(
 )
 
 
-server <- function(input, output) {
+server <- function(input, output, session) {
     
+    #Creating a select button that updates degree options based on major choice
     observe({
-        updateSelectInput(session, "degree", choices = as.character(pseo[pseo$label==input$major, label_degree_level]))
+        options <- pseo %>%
+            group_by(label) %>% 
+            filter(status_y1_earnings == "1", status_y5_earnings == "1", status_y10_earnings == "1", label == input$major) %>% 
+            distinct(label_degree_level)
+        options <- as.character(as_vector(options$label_degree_level))
+        updateSelectInput(session = session, inputId = "degree", choices = options)
     })
     
-    observeEvent(input$submit, {
-        if(nrow(
-            pseo %>% 
-            filter(label == input$major, label_degree_level == input$degree)) == 0){
-            shinyalert(title = "error",
-                       text = "data does not exist",
-                       type = "error")
-        }
-    })
     
     output$selected <- eventReactive(input$submit,{ 
         paste("You have selected: ", input$major)
     })
     
+    #Getting the description of the program
     output$info <- eventReactive(input$submit, { 
         def <- pseo[(pseo$label == input$major), "CIPDefinition"]
         def <- as.character(as.vector(def[1,]))
@@ -72,12 +70,22 @@ server <- function(input, output) {
             summarise(medy1 =  mean(as.numeric(y1_p50_earnings)),
                       medy5 =  mean(as.numeric(y5_p50_earnings)),
                       medy10 =  mean(as.numeric(y10_p50_earnings))) 
+        
         plot_data_long <- plot_data %>% 
             pivot_longer(cols = c(medy1, medy5, medy10), names_to = "Type", values_to = "Amount")
+        
+        #making Type a factor so it plots in the correct order
+        plot_data_long <- plot_data_long %>% 
+            mutate(Type = factor(Type, levels = c("medy1", "medy5", "medy10"))) %>% 
+            mutate(Amount = round(Amount, 0))
+        
         plot1 <- ggplot(plot_data_long, mapping = aes(x = Type, y = Amount, fill = Type)) +
-            geom_bar(stat = "identity") +
-            scale_y_continuous(limit = c(0, 120000)) + scale_fill_brewer(palette = "Set2") +
-            labs(title = input$major)
+            geom_bar(stat = "identity", position = "dodge") +
+            geom_text(aes(label = Amount), position = position_dodge(width = 0.9), vjust = -0.25) +
+            theme(legend.position = "none") +
+            scale_y_continuous(limit = c(0, 155000)) + scale_fill_brewer(palette = "Set2") +
+            labs(title = str_c("Earnings Data for ", input$major), x = "Years after Graduation", y = "Average Median Earnings")
+        
         output$salaries <- renderPlot({
             plot1
         })
